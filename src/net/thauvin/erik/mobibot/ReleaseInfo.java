d5669d4cdb0e7c633b2a3deeb21414ec9b34b435
erik <erik@thauvin.net>
2012-07-06 01:47:25 -0700

    Automatically fetches the title of a URL submission if not already specified. Fixed delicious tags. (comma delimited) Added submitter's nick to tags.

--- src/net/thauvin/erik/mobibot/ReleaseInfo.java
+++ src/net/thauvin/erik/mobibot/ReleaseInfo.java
@@ -1,5 +1,5 @@
 /* Created by JReleaseInfo AntTask from Open Source Competence Group */
-/* Creation date Mon Oct 04 00:59:22 PDT 2010 */
+/* Creation date Fri Jun 29 06:58:32 PDT 2012 */
 package net.thauvin.erik.mobibot;
 
 import java.util.Date;
@@ -20,21 +20,21 @@ public class ReleaseInfo {
    }
 
 
-   /** buildDate (set during build process to 1286179162570L). */
-   private static final Date buildDate = new Date(1286179162570L);
+   /** buildDate (set during build process to 1340978312640L). */
+   private static final Date buildDate = new Date(1340978312640L);
 
    /**
-    * Get buildDate (set during build process to Mon Oct 04 00:59:22 PDT 2010).
+    * Get buildDate (set during build process to Fri Jun 29 06:58:32 PDT 2012).
     * @return Date buildDate
     */
    public static Date getBuildDate() { return buildDate; }
 
 
    /**
-    * Get buildNumber (set during build process to 2).
+    * Get buildNumber (set during build process to 3).
     * @return int buildNumber
     */
-   public static int getBuildNumber() { return 2; }
+   public static int getBuildNumber() { return 3; }
 
 
    /** project (set during build process to "mobibot"). */
