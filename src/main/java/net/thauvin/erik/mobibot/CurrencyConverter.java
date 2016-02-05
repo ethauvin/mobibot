e1685ba706f727d012fde707262694783c115f38
Erik C. Thauvin <erik@thauvin.net>
2016-02-05 12:53:03 -0800

    Updated dependencies.

--- src/main/java/net/thauvin/erik/mobibot/CurrencyConverter.java
+++ src/main/java/net/thauvin/erik/mobibot/CurrencyConverter.java
@@ -31,11 +31,11 @@
  */
 package net.thauvin.erik.mobibot;
 
-import org.jdom.Document;
-import org.jdom.Element;
-import org.jdom.JDOMException;
-import org.jdom.Namespace;
-import org.jdom.input.SAXBuilder;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.Namespace;
+import org.jdom2.input.SAXBuilder;
 
 import java.io.IOException;
 import java.net.URL;
