6a1b3976edfcabc218a528c8cff2250eb115f1c2
Erik C. Thauvin <erik@thauvin.net>
2019-07-28 23:14:19 -0700

    Version 0.7.3-beta+471

--- src/generated/java/net/thauvin/erik/mobibot/ReleaseInfo.java
+++ src/generated/java/net/thauvin/erik/mobibot/ReleaseInfo.java
@@ -14,13 +14,13 @@ import java.time.*;
 public final class ReleaseInfo {
     public static final String PROJECT = "mobibot";
     public static final LocalDateTime BUILDDATE =
-        LocalDateTime.ofInstant(Instant.ofEpochMilli(1564355149782L), ZoneId.systemDefault());
+        LocalDateTime.ofInstant(Instant.ofEpochMilli(1564380207330L), ZoneId.systemDefault());
     public static final int MAJOR = 0;
     public static final int MINOR = 7;
     public static final int PATCH = 3;
     public static final String PRERELEASE = "beta";
-    public static final String BUILDMETA = "468";
-    public static final String VERSION = "0.7.3-beta+468";
+    public static final String BUILDMETA = "471";
+    public static final String VERSION = "0.7.3-beta+471";
 
     /**
      * Disables the default constructor.
