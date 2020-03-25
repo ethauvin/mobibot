a435f0be4954c68c4cfc727d1a506d28e5801303
Erik C. Thauvin <erik@thauvin.net>
2020-03-25 00:20:15 -0700

    Cleanup.

--- src/generated/java/net/thauvin/erik/mobibot/ReleaseInfo.java
+++ src/generated/java/net/thauvin/erik/mobibot/ReleaseInfo.java
@@ -14,13 +14,13 @@ import java.time.*;
 public final class ReleaseInfo {
     public static final String PROJECT = "mobibot";
     public static final LocalDateTime BUILDDATE =
-        LocalDateTime.ofInstant(Instant.ofEpochMilli(1585029881513L), ZoneId.systemDefault());
+        LocalDateTime.ofInstant(Instant.ofEpochMilli(1585119580379L), ZoneId.systemDefault());
     public static final int MAJOR = 0;
     public static final int MINOR = 7;
     public static final int PATCH = 3;
     public static final String PRERELEASE = "beta";
-    public static final String BUILDMETA = "714";
-    public static final String VERSION = "0.7.3-beta+714";
+    public static final String BUILDMETA = "760";
+    public static final String VERSION = "0.7.3-beta+760";
 
     /**
      * Disables the default constructor.
