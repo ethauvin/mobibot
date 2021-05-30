3f628e58f02399ca4adf435218fb52b8af25110f
Erik C. Thauvin <erik@thauvin.net>
2021-05-29 22:01:18 -0700

    Updated PMD rules.

--- src/main/java/net/thauvin/erik/mobibot/modules/War.java
+++ src/main/java/net/thauvin/erik/mobibot/modules/War.java
@@ -52,10 +52,10 @@ public final class War extends AbstractModule {
     // War command
     private static final String WAR_CMD = "war";
     // Deck of card
-    private static final String[] WAR_DECK =
-            new String[]{"Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"};
+    private static final String[] WAR_DECK = 
+        {"Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"};
     // Suits for the deck of card
-    private static final String[] WAR_SUITS = new String[]{"Hearts", "Spades", "Diamonds", "Clubs"};
+    private static final String[] WAR_SUITS = {"Hearts", "Spades", "Diamonds", "Clubs"};
 
     /**
      * The default constructor.
