6ec39c6d6df7367760f64de1c3f79286268ef3e6
Erik C. Thauvin <erik@thauvin.net>
2023-01-28 23:19:12 -0800

    Updated copyright

--- src/main/kotlin/net/thauvin/erik/mobibot/modules/RockPaperScissors.kt
+++ src/main/kotlin/net/thauvin/erik/mobibot/modules/RockPaperScissors.kt
@@ -1,8 +1,7 @@
 /*
  * RockPaperScissors.kt
  *
- * Copyright (c) 2004-2022, Erik C. Thauvin (erik@thauvin.net)
- * All rights reserved.
+ * Copyright 2004-2023 Erik C. Thauvin (erik@thauvin.net)
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
