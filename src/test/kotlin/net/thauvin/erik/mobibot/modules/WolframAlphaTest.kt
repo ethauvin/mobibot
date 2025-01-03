f58951846635baa4532115ed8d4edd586aaa300e
Erik C. Thauvin <erik@thauvin.net>
2025-01-02 18:00:17 -0800

    chore: Update copyright notices for 2025

--- src/test/kotlin/net/thauvin/erik/mobibot/modules/WolframAlphaTest.kt
+++ src/test/kotlin/net/thauvin/erik/mobibot/modules/WolframAlphaTest.kt
@@ -1,7 +1,7 @@
 /*
  * WolframAlphaTest.kt
  *
- * Copyright 2004-2024 Erik C. Thauvin (erik@thauvin.net)
+ * Copyright 2004-2025 Erik C. Thauvin (erik@thauvin.net)
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
