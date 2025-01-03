f05ab9751361758d695f12d2a6afd24dd4003fb6
Erik C. Thauvin <erik@thauvin.net>
2025-01-02 17:59:13 -0800

    build: Bump JokeApi to version 1.0.0-SNAPSHOT

--- src/bld/java/net/thauvin/erik/MobibotBuild.java
+++ src/bld/java/net/thauvin/erik/MobibotBuild.java
@@ -116,7 +116,7 @@ public class MobibotBuild extends Project {
                 .include(dependency("org.jsoup", "jsoup", "1.18.3"))
                 // Thauvin
                 .include(dependency("net.thauvin.erik", "cryptoprice", "1.0.3-SNAPSHOT"))
-                .include(dependency("net.thauvin.erik", "jokeapi", "0.9.2-SNAPSHOT"))
+                .include(dependency("net.thauvin.erik", "jokeapi", "1.0.0-SNAPSHOT"))
                 .include(dependency("net.thauvin.erik", "pinboard-poster", "1.1.2-SNAPSHOT"))
                 .include(dependency("net.thauvin.erik.urlencoder", "urlencoder-lib-jvm", "1.6.0"));
         scope(test)
