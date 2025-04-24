b4754f13115e16a10073bdef12eeacd0152174af
Erik C. Thauvin <erik@thauvin.net>
2025-04-24 11:32:11 -0700

    Bump Gson to version 2.13.1

--- src/bld/java/net/thauvin/erik/MobibotBuild.java
+++ src/bld/java/net/thauvin/erik/MobibotBuild.java
@@ -92,7 +92,7 @@ public class MobibotBuild extends Project {
                 .include(dependency("commons-codec", "commons-codec", "1.18.0"))
                 .include(dependency("commons-net", "commons-net", "3.11.1"))
                 // Google
-                .include(dependency("com.google.code.gson", "gson", "2.13.0"))
+                .include(dependency("com.google.code.gson", "gson", "2.13.1"))
                 .include(dependency("com.google.guava", "guava", "33.2.1-jre"))
                 // Kotlin
                 .include(dependency("org.jetbrains.kotlin", "kotlin-stdlib", kotlin))
