7617a9a9e3515be38687cf92ccbbb001230ceacc
Erik C. Thauvin <erik@thauvin.net>
2025-06-29 10:13:55 -0700

    Update to new sonarqube-scan-action

--- src/main/kotlin/net/thauvin/erik/mobibot/ReleaseInfo.kt
+++ src/main/kotlin/net/thauvin/erik/mobibot/ReleaseInfo.kt
@@ -14,12 +14,12 @@ import java.time.ZoneId
  */
 object ReleaseInfo {
     const val PROJECT = "mobibot"
-    const val VERSION = "0.8.0-rc+20250516012000"
+    const val VERSION = "0.8.0-rc+20250616222735"
 
     @JvmField
     @Suppress("MagicNumber")
     val BUILD_DATE: LocalDateTime = LocalDateTime.ofInstant(
-        Instant.ofEpochMilli(1747383600399L), ZoneId.systemDefault()
+        Instant.ofEpochMilli(1750138055583L), ZoneId.systemDefault()
     )
 
     const val WEBSITE = "https://mobitopia.org/mobibot/"
