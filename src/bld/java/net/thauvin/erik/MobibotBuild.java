85dbc0436c81ed62c1ea8ad60ec729ea0390419a
Erik C. Thauvin <erik@thauvin.net>
2025-07-07 11:39:08 -0700

    Bump JUnit to version 5.13.3

--- src/bld/java/net/thauvin/erik/MobibotBuild.java
+++ src/bld/java/net/thauvin/erik/MobibotBuild.java
@@ -133,9 +133,9 @@ public class MobibotBuild extends Project {
                 .include(dependency("com.willowtreeapps.assertk", "assertk-jvm", version(0, 28, 1)))
                 // JUnit
                 .include(dependency("org.jetbrains.kotlin", "kotlin-test-junit5", kotlin))
-                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 13, 2)))
-                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 13, 2)))
-                .include(dependency("org.junit.platform", "junit-platform-launcher", version(1, 13, 2)));
+                .include(dependency("org.junit.jupiter", "junit-jupiter", version(5, 13, 3)))
+                .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1, 13, 3)))
+                .include(dependency("org.junit.platform", "junit-platform-launcher", version(1, 13, 3)));
 
         List<String> jars = new ArrayList<>();
         runtimeClasspathJars().forEach(f -> jars.add("./lib/" + f.getName()));
