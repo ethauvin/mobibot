272eb3e35995c5145be8cd846a4f7752d54105c2
Erik C. Thauvin <erik@thauvin.net>
2025-09-11 11:01:28 -0700

    Bump JSoup to version 1.21.2

--- src/bld/java/net/thauvin/erik/MobibotBuild.java
+++ src/bld/java/net/thauvin/erik/MobibotBuild.java
@@ -119,7 +119,7 @@ public class MobibotBuild extends Project {
                         .exclude("com.squareup.okhttp3", "okhttp"))
                 .include(dependency("net.objecthunter", "exp4j", "0.4.8"))
                 .include(dependency("org.json", "json", "20250517"))
-                .include(dependency("org.jsoup", "jsoup", "1.21.1"))
+                .include(dependency("org.jsoup", "jsoup", "1.21.2"))
                 // Thauvin
                 .include(dependency("net.thauvin.erik", "cryptoprice", "1.0.3-SNAPSHOT"))
                 .include(dependency("net.thauvin.erik", "frankfurter4j", "0.9.0-SNAPSHOT"))
