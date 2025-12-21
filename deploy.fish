1c3157393623cd6e173caa584e0d54ba057104c0
Erik C. Thauvin <erik@thauvin.net>
2025-12-21 14:53:29 -0800

    Use mscp and ssh instead of sftp

--- deploy.fish
+++ deploy.fish
@@ -1,11 +1,16 @@
 #!/usr/bin/env fish
 
+set nix4 'erik@nix4'
+set loc /home/mobibot/mobitopia/mobibot
+set mobibot 'mobibot.jar'
+
 ./bld clean jar deploy
 if test $status -eq 0
-    echo "cd /home/mobibot/mobitopia/mobibot
-lcd deploy
-put *.jar
-cd lib
-rm *.jar
-put lib/*.jar" | sftp nix4
+    echo "Deleting old jars..."
+    ssh $nix4 "rm -rf $loc/$mobibot \"$loc/lib/*.jar\""
+    echo "Uploading new jars..."
+    mscp deploy/lib/*.jar $nix4:$loc/lib/
+    scp deploy/$mobibot $nix4:$loc
+    echo "Updating permissions..."
+    ssh $nix4 "chmod 755 $loc/$mobibot"
 end
