eaeb467ffe40c196c328dbf3d853d9ab6ac49ede
Erik C. Thauvin <erik@thauvin.net>
2026-02-28 00:12:52 -0800

    Bump Testing Helpers to version 0.9.6

--- src/bld/java/net/thauvin/erik/MobibotBuild.java
+++ src/bld/java/net/thauvin/erik/MobibotBuild.java
@@ -134,7 +134,7 @@ public class MobibotBuild extends Project {
         scope(test)
                 // bld
                 .include(dependency("com.uwyn.rife2", "bld-extensions-testing-helpers",
-                        version(0, 9, 5)))
+                        version(0, 9, 6)))
                 // Mockito
                 .include(dependency("net.bytebuddy", "byte-buddy",
                         version(1, 18, 4)))
